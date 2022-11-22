d1a59dcea0ad9765ae6f7965be6ccb33cc6429af
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2022-11-23 01:59:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Abdiaziz';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = 'Welcome to Flatbook, '.toUpperCase() + currentUser.toUpperCase() + '!';
+
+const shortGreeting = 'Welcome, '+ currentUser.charAt() + '!';
